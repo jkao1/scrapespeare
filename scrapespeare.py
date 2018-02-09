7aae882a7be7c9c87706ffcbba9613205c4d7188
Jason Kao <jasonkao85@gmail.com>
2018-02-09 07:33:25 -0500

    scene fucntionality

--- scrapespeare.py
+++ scrapespeare.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import urllib2, webbrowser
 import sys
 
-url = "http://nfs.sparknotes.com/lear/"
+url = "http://nfs.sparknotes.com/richardiii/"
 filename = "nofear.html"
 start = int(sys.argv[1])
 end = int(sys.argv[2])
@@ -23,7 +23,7 @@ td.noFear-left { width:50%; }
 </style>
 """
 
-for i in range(start/2, (end+2)/2):
+for i in range(start/2, end/2):
     sceneUrl = url + "page_" + str(i*2) + ".html"
     print("analyzing the contents of %s" % (sceneUrl))
     content = urllib2.urlopen(sceneUrl).read()
